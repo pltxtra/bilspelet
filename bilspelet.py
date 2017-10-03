e9dd710d69104e7a1962c27c81279028ee3954c7
Anton Persson <pltxtra@vuknob.com>
2017-10-03 22:04:03 +0200

    The car keeps the same speed independent of the frame rate.

--- bilspelet.py
+++ bilspelet.py
@@ -19,6 +19,8 @@ for x in range(0, MAX_PICS):
     car_pic.append(rotated)
     car_rct.append(rotated.get_rect())
 
+last_time = pygame.time.get_ticks()
+
 pedal_down = False
 
 direction = 0.0
@@ -27,7 +29,14 @@ position_y = height / 2.0
 rotate_left = False
 rotate_right = False
 
+car_speed = 200.0
+rotate_speed = 200.0
+
 while 1:
+    this_time = pygame.time.get_ticks()
+    delta_time = (this_time - last_time) / 1000.0
+    last_time = this_time
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -47,9 +56,9 @@ while 1:
                 rotate_right = False
 
     if rotate_left:
-        direction = direction + 0.5
+        direction = direction + rotate_speed * delta_time
     elif rotate_right:
-        direction = direction - 0.5
+        direction = direction - rotate_speed * delta_time
 
     if direction >= 360.0:
         direction -= 360.0
@@ -59,8 +68,8 @@ while 1:
     picture_index = int(direction * MAX_PICS / 360.0)
     angle = (picture_index / MAX_PICS) * 2.0 * math.pi
     if pedal_down:
-        position_x = position_x + math.cos(angle)
-        position_y = position_y - math.sin(angle)
+        position_x = position_x + car_speed * delta_time * math.cos(angle)
+        position_y = position_y - car_speed * delta_time * math.sin(angle)
 
     position = (int(position_x), int(position_y))
     rect = car_rct[picture_index]
